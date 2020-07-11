613e203edcbaa3e4db99a16cdd423e5b0c7e2386
龚箭 <15031617367@163.com>
2020-07-11 09:05:54 +0800

    master UPdate

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,6 @@
 cnpm run build &&
 git checkout dist &&
 rm -rf build config src .babelrc .editorconfig .postcssrc.js *.html *.json *.md &&
-mv dist/* ./ &&
 git add . && 
 git commit -m "update" &&
 git push origin dist &&
